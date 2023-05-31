147e80b233d9dbadc9256060b0fdb53889caffa1
Julian <jdegut@hotmail.fr>
2023-06-01 00:38:56 +0200

    Final MAJ : HowToUse + Parameters

--- src/Main.java
+++ src/Main.java
@@ -16,40 +16,56 @@ import java.util.stream.IntStream;
 public class Main {
 
 	/**
+	 * TODO Parameters to change
+	 */
+	private static final int MAX_ITERATIONS = 10000;
+	private static final int MAX_NEIHGBORS = 250;
+	private static final int TABU_LIST_SIZE = 10;
+	private static final double FINAL_TEMPERATURE = 0.01;
+	private static final double COOLING_RATE = 0.9;
+
+	/**
+	 * TODO Algorithm to execute
+	 */
+	private static final Class<? extends VRPTW> CHOSEN_ALGORITHM = SimulatedAnnealing.class;
+
+	/**
 	 * Main method : execute the program
 	 */
 	public static void main(String[] args) {
 
-		Data data = new Data("./Data/data111.vrp");
+		Data data = new Data("./Data/data1202.vrp");
+		// TODO Uncomment to execute the linear program
 //		LinearMP.solve(data);
-		executeAlgorithm(data, Tabu.class);
+		// TODO Uncomment to execute the chosen algorithm
+		executeAlgorithm(data);
+		// TODO Uncomment to get the best optimum for a data file
 //		getOptimumForAData(data,10);
-
 	}
 
 	/**
 	 * Execute an algorithm on a data file
+	 *
 	 * @param data data file
-	 * @param algorithm algorithm to execute (class)
 	 */
-	private static void executeAlgorithm(Data data, Class<? extends VRPTW> algorithm) {
+	private static void executeAlgorithm(Data data) {
 		Solution solution = VRPTW.init(data);
 		int oldCost = (int) solution.getTotalDistance();
 		int oldNbVehicles = solution.getVehicles().size();
 
 		Solution end;
-		if (algorithm.equals(Randomizer.class)) {
+		if (CHOSEN_ALGORITHM.equals(Randomizer.class)) {
 			System.out.println("Randomizer algorithm found");
-			end = new Randomizer(1000).search(solution, true);
-		} else if (algorithm.equals(HillClimbing.class)) {
+			end = new Randomizer(MAX_ITERATIONS).search(solution, true);
+		} else if (CHOSEN_ALGORITHM.equals(HillClimbing.class)) {
 			System.out.println("HillClimbing algorithm found");
-			end = new HillClimbing(100).search(solution, true);
-		} else if (algorithm.equals(Tabu.class)) {
+			end = new HillClimbing(MAX_NEIHGBORS).search(solution, true);
+		} else if (CHOSEN_ALGORITHM.equals(Tabu.class)) {
 			System.out.println("Tabu algorithm found");
-			end = new Tabu(10, 1000, 100).search(solution, true);
-		} else if (algorithm.equals(SimulatedAnnealing.class)) {
+			end = new Tabu(TABU_LIST_SIZE, MAX_ITERATIONS, MAX_NEIHGBORS).search(solution, true);
+		} else if (CHOSEN_ALGORITHM.equals(SimulatedAnnealing.class)) {
 			System.out.println("SimulatedAnnealing algorithm found");
-			end = new SimulatedAnnealing(0.1, 1000, 0.9).search(solution, true);
+			end = new SimulatedAnnealing(FINAL_TEMPERATURE, MAX_ITERATIONS, COOLING_RATE).search(solution);
 		} else {
 			System.out.println("Algorithm not found");
 			return;
@@ -70,10 +86,9 @@ public class Main {
 	private static void getOptimumForAData(Data data, int nbIterations) {
 		ArrayList<Solution> solutions = new ArrayList<>();
 		Date start = new Date();
-
 		IntStream.range(0, nbIterations).parallel().forEach(i -> {
 			Solution solution = VRPTW.getRandomSolution(data);
-			solution = new SimulatedAnnealing(0.1, 1000, 0.9).search(solution);
+			solution = new SimulatedAnnealing(FINAL_TEMPERATURE, MAX_ITERATIONS, COOLING_RATE).search(solution);
 			synchronized (solutions) {
 				solutions.add(solution);
 			}
